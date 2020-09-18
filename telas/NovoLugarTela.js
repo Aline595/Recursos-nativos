90d0b37de6db54c637af1003ae4f5e54d632b1d4
unknown <aline.as385@gmail.com>
2020-09-18 16:50:52 -0300

    novoLugar

--- telas/NovoLugarTela.js
+++ telas/NovoLugarTela.js
@@ -1,20 +1,45 @@
 import React from 'react';
 import {
   View,
+  ScrollView,
+  TextInput,
+  Button,
   StyleSheet,
   Text
 } from 'react-native';
 
+import Cores from "../constantes/Cores";
+
 const NovoLugarTela = (props) => {
   return (
-    <View>
-      <Text>NovoLugarTela</Text>
-    </View>
+    <ScrollView>
+      <View style={estilos.form}>
+        <Text style={estilos.titulo}>Novo lugar</Text>
+        <TextInput style={estilos.textInput} />
+        <Button
+          title="Salvar Lugar"                    
+          color={Cores.primary}
+          onPress={() => { }}                
+        /> 
+      </View>     
+    </ScrollView>
   )
 }
 
 const estilos = StyleSheet.create({
-
+  form: {
+    margin: 30
+  },
+  titulo:{
+    fontSize: 18,
+    marginBottom: 15
+  },
+  textInput:{
+    borderBottomColor: '#DDD',
+    borderBottomWidth: 2,
+    marginBottom: 15,
+    paddingVertical: 4
+  }
 });
 
 export default NovoLugarTela;
