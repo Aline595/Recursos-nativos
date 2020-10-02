7ed67e191e7c25d749483c440d63d09e8749d8a0
unknown <aline.as385@gmail.com>
2020-10-02 17:51:24 -0300

    conectando com bd

--- store/lugares-reducer.js
+++ store/lugares-reducer.js
@@ -1,4 +1,4 @@
-import { ADD_LUGAR } from "./lugares-actions";
+import { ADD_LUGAR, LISTA_LUGARES } from "./lugares-actions";
 import  Lugar  from '../modelo/Lugar'
 
 const estadoInicial = {
@@ -8,9 +8,14 @@ const estadoInicial = {
 export default (estado = estadoInicial, action) => {
   switch (action.type){
     case ADD_LUGAR:
-      const lugar = new Lugar (new Date().toString(), action.dadosLugar.nomeLugar, action.dadosLugar.imagemURI )
+      const lugar = new Lugar (action.dadosLugar.id.toString(), action.dadosLugar.nomeLugar, action.dadosLugar.imagemURI )
+      //console.log(lugar);
       return {
         lugares: estado.lugares.concat(lugar)
+      }
+    case LISTA_LUGARES:
+      return {
+        lugares: action.lugares.map(l => new Lugar(l.id.toString(), l.nome, l.imagemURI))
       }
     default:
       return estado;
